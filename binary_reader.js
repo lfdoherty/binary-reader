d4b7a770a6ceadffbcd412467d5ba645e41a0ce8
Liam <lfdoherty@gmail.com>
2016-11-11 10:29:15 +0800

    commit

--- binary_reader.js
+++ binary_reader.js
@@ -1,49 +1,70 @@
 
-export class BinaryReader {
+class BinaryReader {
 	constructor(data: Buffer){
 		this.data = data;
 		this.pos = 0
 	}
-	_ensure(int n){
+	_ensure(n){
 		if(this.pos + n > this.data.Length){
 			throw new Error(`not enough data left to read ${n} bytes`)
 		}
 	}
+	takeByte() {
+		this._ensure(1)
+		const result = this.data[this.pos];
+		this.pos++;
+		return result;
+	}
+	takeString(encoding = 'utf8') {
+		const strLen = this.takeInt()
+		return this.takeRawString(strLen, encoding)
+	}
+	takeRawString(len, encoding = 'utf8'){
+		this._ensure(len);
+		const str = this.data.toString(encoding, this.pos, this.pos + len);
+		this.pos += len;
+		return str;
+	}
 	takeDouble() {
 		this._ensure(8);
-		var result = this.data.readDoubleLE(this.pos);
+		const result = this.data.readDoubleLE(this.pos);
 		this.pos += 8;
 		return result;
 	}
 	takeFloat() {
 		this._ensure(4);
-		var result = this.data.readFloatLE(this.pos)
+		const result = this.data.readFloatLE(this.pos)
 		this.pos += 4;
 		return result;
 	}
 	takeInt() {
 		this._ensure(4);
-		var result = this.data.readInt32LE(this.pos)
+		console.log('pos: ' + this.pos)
+		const result = this.data.readInt32LE(this.pos)
 		this.pos += 4;
 		return result;
 	}
-	takeArrayFloat(int n) {
+	takeArrayFloat(n) {
 		this._ensure(n * 4);
 		const result = new Float32Array(n);
-		for(int i=0;i<n;++i){
+		for(let i=0;i<n;++i){
 			result[i] = this.data.readFloatLE(this.pos)
 			this.pos += 4;
 		}
 		return result;
 	}
-	takeArrayUShort(int n) {
+	takeArrayUShort(n) {
 		this._ensure(n * 2);
 		const result = new Uint16Array();
-		for(int i=0;i<n;++i){
-			result[i] = this.data.readUint16LE(this.pos)
+		for(let i=0;i<n;++i){
+			result[i] = this.data.readUInt16LE(this.pos)
 			this.pos += 2;
 		}
 		return result;
 	}
+}
+
+export function make(buf){
+	return new BinaryReader(buf)
 }
 
